ffe7f22e3faab631f0a7d5d6fee8ad4ce4b9f566
m-zishan007 <m.zishan007@gmail.com>
2024-11-22 13:57:57 +0530

    Update index.js
    Contact details updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "E-264 Near Paryas Jahangirpuri Delhi-110033 ",
+  phoneNo: "+91 99995 60102",
+  email: "md.zishan.kh@gmail.com",
 };
